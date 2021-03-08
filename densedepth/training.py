0d5deb7823efe6ddc9989995733b5bfa157a9b4d
soumik12345 <19soumik.rakshit96@gmail.com>
2021-03-08 11:12:35 +0530

    Updated Trainer

--- densedepth/training.py
+++ densedepth/training.py
@@ -3,6 +3,7 @@ import gdown
 import wandb
 import zipfile
 import subprocess
+from typing import List
 import tensorflow as tf
 import tensorflow_addons as tfa
 
@@ -35,11 +36,25 @@ class Trainer:
                 name=self.experiment_name, sync_tensorboard=True
             )
 
-    def compile(self, train_dataset, val_dataset, learning_rate: float, weight_decay: float, strategy):
+    def compile(
+            self, train_dataset, val_dataset, model_build_shape: List[int],
+            learning_rate: float, weight_decay: float, strategy=None):
         self.train_dataset = train_dataset
         self.val_dataset = val_dataset
-        with strategy.scope():
+        if strategy is not None:
+            with strategy.scope():
+                self.model = DenseDepth()
+                self.model.compile(
+                    optimizer=tfa.optimizers.AdamW(
+                        learning_rate=learning_rate,
+                        weight_decay=weight_decay
+                    ), loss=DenseDepthLoss(
+                        lambda_weight=0.1, depth_max_val=1000.0 / 10.0
+                    )
+                )
+        else:
             self.model = DenseDepth()
+            self.model.build(model_build_shape)
             self.model.compile(
                 optimizer=tfa.optimizers.AdamW(
                     learning_rate=learning_rate,
